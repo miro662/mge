f6b895ce39a11c16c1762e8bb78f95cfc6549f34
miro662 <miro662@gmail.com>
2016-06-26 22:52:03 +0200

    Changes in main.c file

--- main.c
+++ main.c
@@ -2,26 +2,11 @@
 
 #include <mge.h>
 
-void start(void* data)
-{
-  printf("starting\n");
-}
-
-void aexit(void* data)
-{
-  printf("exiting\n");
-}
-
 // MGE entry point
 int main(int argc, char* argv[]) {
   // Load systems
   MGE_requireSystem(MGE_S_Oversystem);
   MGE_requireSystem(MGE_S_Events);
-
-  //b
-  MGE_addCallback(&MGE_E_AtStart, start);
-  MGE_addCallback(&MGE_E_AtExit, aexit);
-  //b
 
   // Call start event
   MGE_call(MGE_E_Start, NULL);
