c6280110727e0607ea7962b9ae48d2a31c02f011
miro662 <miro662@gmail.com>
2016-06-26 20:39:35 +0200

    Created main function

---
+++ include/mge/mge.h
@@ -0,0 +1,12 @@
+#ifndef __MGE_H
+#define __MGE_H
+
+// include/mge.h - main MGE include file
+
+// ===== External include files =====
+
+// ----- Standard C library -----
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+// main.c - contains MGE entry point
+
+#include "mge/mge.h"
+
+// MGE entry point
+int main(int argc, char* argv[]) {
+  printf("Hello %s! Hello, %s!\n", "world", "MGE");
+  return 0;
+}
